c783b45fee3a9033034dc6132119f13c522207d9
kartik.kaul <kartik.kaul@rakuten.com>
2021-12-28 17:24:17 +0900

    [spring][JPA]spring JPA tutorial initail commit

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,24 +1,13 @@
 package guru.springframework.sdjpaintro;
 
-import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
-
-    @Autowired
-    BookRepository bookRepository;
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    void testBookRepository(){
-        long count = bookRepository.count();
-        assertThat(count).isGreaterThan(0);
-    }
 }
